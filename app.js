b5fa8db1cbdc8254eb29fb63a8483fc1ed1852e7
jack-borg <jacob.fam.borg@gmail.com>
2019-05-29 21:43:57 +0200

    changed path

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 // app.use("/users", usersRouter);
 // app.use("/api", apirouter);
-app.use("/graphQL", cors(), graphQL);
+app.use("/", cors(), graphQL);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
